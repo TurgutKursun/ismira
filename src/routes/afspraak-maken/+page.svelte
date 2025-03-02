dc239a0734cffe95ae03adf2d8c4bf860b4c226c
TurgutKursun <turgut.reis.kursun@gmail.com>
2025-03-02 19:22:45 +0100

    maps are added

--- src/routes/afspraak-maken/+page.svelte
+++ src/routes/afspraak-maken/+page.svelte
@@ -12,6 +12,11 @@ import Heading from "$lib/components/Heading.svelte";
         flex-direction: column;
         align-items: center;
     }
+
+    .map {
+        max-width: 200px;
+        max-height: 200px;
+    }
 </style>
 
 
@@ -19,14 +24,23 @@ import Heading from "$lib/components/Heading.svelte";
     <Heading headingOne="Ismira" headingTwo="Afspraak maken"/>
     <Paragraph paragraphText="Om een afspraak te maken, of voor <br/> meer informatie, kan u ons altijd </br> bereiken via e-mail, sms, of gsm"/>
 
-    <span class="material-icons-outlined" style="font-size: 36px;">mail</span>
+    <span class="material-icons-outlined" style="font-size: 40px;">mail</span>
     <Email email="info@ismira.com" --padding-bottom=20px/>
     <Paragraph paragraphText="We beantwoorden uw mails <br/> zo snel mogelijk, vaak binnen de 24u" --padding-top=0px/>
 
-    <span class="material-icons-outlined" style="font-size: 36px;">phone</span>
+    <span class="material-icons-outlined" style="font-size: 40px;">phone</span>
     <PhoneNumber number="+32494130449" --padding-bottom=20px/>
     <Paragraph paragraphText="Bereikbaar maandag - vrijdag <br/> tussen 09h00 en 16h00" --padding-top=0px/>
 
-    <span class="material-icons-outlined" style="font-size: 36px;">location_on</span>
+    <span class="material-icons-outlined" style="font-size: 40px;">location_on</span>
     <Address address="Brussel, Zaventem, Vlinderstraat 28" --padding-bottom=20px/>
+    <iframe 
+        title=""
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2512.5574669225693!2d4.5052269772370055!3d50.96888567169585!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3e6df12b7d241%3A0xc166ae1c165cc91a!2sVlinderstraat%2C%201982%20Zemst!5e0!3m2!1sen!2sbe!4v1740938947066!5m2!1sen!2sbe" 
+        width="90%" 
+        height="300px" 
+        style="padding-top=0px; border:0; border-radius: 32px; margin-bottom: 30px"  
+        loading="lazy" 
+        referrerpolicy="no-referrer-when-downgrade">
+    </iframe>
 </div>
