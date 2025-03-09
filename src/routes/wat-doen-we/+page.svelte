cc0259ed7c820fe33626650c65a4d8990f6df218
TurgutKursun <turgut.reis.kursun@gmail.com>
2025-03-09 15:47:39 +0100

    response wat-doen-we

--- src/routes/wat-doen-we/+page.svelte
+++ src/routes/wat-doen-we/+page.svelte
@@ -4,12 +4,21 @@
 </script>
 
 <style>
-
+    @media(min-width: 512px) {
+        .accordions {
+            width: 40%;
+            display: flex;
+            flex-direction: column;
+            margin: 0 auto;
+            margin-top: 30px 
+        }
+    }
 </style>
 
 <Heading headingOne="Ismira" headingTwo="Wat doen we?"/>
 
-<Accordion 
+<div class="accordions">
+    <Accordion 
     icon="diversity_1"
     title="Therapie" 
     text="We bieden begeleiding bij psychische moeilijkheden en persoonlijke groei. Samen werken we aan meer welzijn. <br/> <br/> Voor therapie kan u terecht bij:"
@@ -22,7 +31,7 @@
         }]}
         />
 
-<Accordion
+    <Accordion
     icon="toys_and_games"
     title="EMDR"
     text="Met EMDR helpen we bij het verwerken van trauma en ingrijpende ervaringen. Met gerichte technieken verminderen we de emotionele impact van herinneringen. <br/> <br/> Voor EMDR kan u terecht bij:"
@@ -35,7 +44,7 @@
         }]}
     />
 
-<Accordion
+    <Accordion
     icon="person_search"
     title="Diagnostiek"
     text="We voeren psychologische onderzoeken uit om inzicht te krijgen in klachten en sterktes. Dit helpt om de juiste ondersteuning en begeleiding te bieden. <br/> <br/> Voor diagnostiek kan u terecht bij:"
@@ -48,7 +57,7 @@
         }]}
     />
 
-<Accordion
+    <Accordion
     icon="toys_and_games"
     title="Kinderen"
     text="We ondersteunen kinderen bij emotionele en gedragsmatige uitdagingen. We streven naar een veilige omgeving waarin ze zich optimaal kunnen ontwikkelen. <br/> <br/> Voor begeleiding van kinderen kan u terecht bij:"
@@ -60,3 +69,4 @@
             lastName: "Zerrouk"
         }]}
     />
+</div>
