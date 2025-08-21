22a24d22de264240fcad1a6193bbbf54491bcf0b
TurgutKursun <turgut.reis.kursun@gmail.com>
2025-08-21 14:22:11 +0200

    test

--- src/routes_for_later/online-afspraak-maken/+page.svelte
+++ src/routes_for_later/online-afspraak-maken/+page.svelte
@@ -6,7 +6,7 @@
 
 </style>
 
-<Heading headingOne="Ismira"  headingTwo="Online Afspraak Maken"/>
+<Heading headingOne="Ismira "  headingTwo="Online Afspraak Maken"/>
 
 <!-- Google Calendar Appointment Scheduling begin -->
 <iframe src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ27J89xLqMpYSJ8Lf9C8bmDdbki5Ccpl9Nul6yLrImP3ft6pcrRx0UTc2tWbibCfDgf5OL8voDb?gv=true" style="border: 0" width="100%" height="1200" frameborder="0"></iframe>
